c9e9f5fc0618a7efd44c2c5e520980ffec5f9c9f
tracer-ics2018 <tracer@njuics.org>
2019-05-08 20:19:38 +0800

    >  compile 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  20:19:38 up 1 day, 51 min,  1 user,  load average: 4.83, 4.95, 4.90 af081cdc95dab3c6df615875e59c0753f12e330f

--- nemu/include/common.h
+++ nemu/include/common.h
@@ -39,7 +39,35 @@ typedef uint16_t ioaddr_t;
 
 
 #include <stdarg.h>
+/*
 void inline printf_debug(const char *format, ...);
 void inline QUESTION(char *str);
+*/
+
+void inline printf_debug(const char *format, ...) {
+    #ifdef DBG
+
+    printf("%s\\ ", __FILE__);
+    printf("%s\\ ", __func__);
+    printf("%d:\t", __LINE__);
+    
+
+    va_list args;
+    va_start(args, format);
+
+    vprintf(format, args);
+
+    va_end(args);
+    #endif
+}
+
+
+
+void inline QUESTION(char *str) {
+    printf("%s\\ ", __FILE__);
+    printf("%s\\ ", __func__);
+    printf("%d:\t", __LINE__);
+    printf("%s\n", str);
+}
 
 #endif
