25b88357cb1888250fee528264de1a20d986ebe9
tracer-ics2018 <tracer@njuics.org>
2019-05-10 09:28:01 +0800

    >  compile 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  09:28:01 up 2 days, 13:59,  1 user,  load average: 1.69, 1.52, 1.52 c0773d55b833ed726b2203b6c213e0063796f91b

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -157,8 +157,8 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
   
-  
-  rtl_xori(&dest, &src1, ~0);
+
+  rtl_xori(dest, src1, ~0);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
