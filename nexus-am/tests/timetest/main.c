041e564de5ae499a503cc9df9ac613c05e1021da
tracer-ics2018 <tracer@njuics.org>
2019-05-18 12:49:08 +0800

    >  gdb 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  12:49:08 up 3 days,  2:25,  1 user,  load average: 1.09, 1.03, 0.92 188b5d25bf269af338884db89feb3527154d5ae8

--- nexus-am/tests/timetest/main.c
+++ nexus-am/tests/timetest/main.c
@@ -20,7 +20,7 @@ int main(){
 
   while (1) {
     while(uptime() < 1000 * sec) {
-      printf("%d\n", uptime());
+      ;
     }
     get_timeofday(&rtc);
     printf("%d-%d-%d %02d:%02d:%02d GMT (", rtc.year, rtc.month, rtc.day, rtc.hour, rtc.minute, rtc.second);
