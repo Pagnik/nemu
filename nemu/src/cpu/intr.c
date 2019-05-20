d8dfcdac1f7a833d9304fad93f89a3acabf7a72b
tracer-ics2018 <tracer@njuics.org>
2019-05-20 13:15:59 +0800

    >  compile 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  13:15:59 up 7 days,  3:04,  2 users,  load average: 0.26, 0.71, 0.74 ec26c39af668cd91ff512e17d500f6d903c1747

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,11 +7,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
   // TODO();
-  rtl_push(cpu.eflags);
-  rtl_push(cpu.cs);
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.cs);
   //rtl_push(cpu.eip);
-  rtl_push(ret_addr);
-  
+  rtl_push(&ret_addr);
+  // TODO: check NO < limits , then what to do?
   GateDesc *idt = vaddr_read(cpu.idtr.base, 4);
   rtl_j(idt[NO].offset_31_16);
 }
