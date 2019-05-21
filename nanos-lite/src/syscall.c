2cb5ee33f61d43d98e4c4cd8d97e6248131227c5
tracer-ics2018 <tracer@njuics.org>
2019-05-21 21:58:09 +0800

    >  gdb 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  21:58:09 up 3 days,  6:44,  1 user,  load average: 3.36, 3.06, 2.90 a332185a739be007ae1fc18df9cc2ee5c9f203fb

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -12,15 +12,36 @@ _Context* do_syscall(_Context *c) {
     case SYS_yield: {
 
       _yield();
+
+      // QUESTION: what's the point to set GPRx? 
+      // since we can just pass the result automatically by c compiler
+      // it's in $EAX
+      // or it's just a manner.
       c->GPRx = 0;
       break;
     }
 
     case SYS_exit: {
-      for (int i = 0; i < 4; i ++) {
+      /*for (int i = 0; i < 4; i ++) {
         printf("gpr: %d\n", a[i]);
-      }
+      }*/
       _halt(a[1]);
+      break;
+    }
+
+    case SYS_write: {
+      int fd = a[1];
+      char *buf = (char *) a[2];
+      int count = a[3];
+      // a[1] = fd
+      // a[2] = buf[]
+      // a[3] = count
+      if (fd == 1 || fd == 2) {
+        for (int i = 0; i < count; i++) {
+          _putc(buf[i]);
+        }
+      }
+      c->GPRx = count;
       break;
     }
     default: panic("Unhandled syscall ID = %d", a[0]);
