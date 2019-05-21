1441999c5918f3aad7f828372a30b53c3fb93e31
tracer-ics2018 <tracer@njuics.org>
2019-05-21 20:41:04 +0800

    >  gdb 171220000 tumblingdice Linux tumblingdice-PC 4.15.0-30deepin-generic #31 SMP Fri Nov 30 04:29:02 UTC 2018 x86_64 GNU/Linux  20:41:04 up 3 days,  5:27,  1 user,  load average: 3.14, 2.84, 2.75 b584ceefd168d8bdf08f1b72208fd84133e4e949

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -5,7 +5,7 @@ static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void vectrap();
 void vecnull();
-
+void vecsys();
 
 
 /*
@@ -79,7 +79,7 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
   // -------------------- system call --------------------------
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
-
+  idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_KERN);
   set_idt(idt, sizeof(idt));
 
   // register event handler
